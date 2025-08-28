a851b99cafba8ef20f523dd866b705c2d0215b44
jonascjak <82451274+jonascjak@users.noreply.github.com>
2025-08-28 15:17:47 +0200

    Update repositories.py

--- repositories.py
+++ repositories.py
@@ -1,2 +1,22 @@
-GROUP_REPOS = [
+'GROUP_REPOS = [
+    {
+        "name": "Example Group",
+        "gitLinks": ["https://<git_link>"],
+        "backend": "http(s)://<IP_DOMAIN>/<APIURL>",
+        "frontend": "http(s)://<IP_DOMAIN>/<FrontEndURL>",
+        "monitoring": "http(s)://<IP_DOMAIN>/<MonitoringURL>",
+        "stack": ["Flask", "Svelte", "CouchDB", "Redis"],
+        "documentation": ["link to documentation", "another link if it applies", "et cetera"],
+        "sla": "link to sla",
+    },
+    {
+        "name": "fullmoon_monkey",
+        "gitLinks": ["https://github.com/orgs/fullmoonmonkey/repositories"],
+        "backend": "",
+        "frontend": "",
+        "monitoring": "",
+        "stack": [],
+        "documentation": [],
+        "sla": "",
+    }
 ]
